7f3fee45c108a26ca7c18582b1dfbc268c018dcc
kimdhoe <kimdhoe1@gmail.com>
2017-01-23 20:51:41 +0900

    Inititialize repository

---
+++ src/index.js
@@ -0,0 +1,156 @@
+// A JongseongCode is an integer[0, 27].
+//   - There are 27 possible jongseong.
+//     code  |  e.g.
+//    -------+------
+//       0   |   가
+//       1   |   각
+//       2   |   갂
+//       3   |   갃
+//       4   |   간
+//       5   |   갅
+//       6   |   갆
+//       7   |   갇
+//       8   |   갈
+//       9   |   갉
+//      10   |   갊
+//      11   |   갋
+//      12   |   갌
+//      13   |   갍
+//      14   |   갎
+//      15   |   갏
+//      16   |   감
+//      17   |   갑
+//      18   |   값
+//      19   |   갓
+//      20   |   갔
+//      21   |   강
+//      22   |   갖
+//      23   |   갗
+//      24   |   갘
+//      25   |   같
+//      26   |   갚
+//      27   |   갛
+
+// jongseongHangul :: string -> JongseongCode
+// Given a Hangul letter, computes its jongseong code.
+// Assume h is a single letter Hangul string (가-힣).
+//   - 44032 (AC00) is the code point of 가, which is the first Hangul letter.
+//   - 28 = the number of the jongseongs + 1
+const jongseongHangul = h =>
+  (h.charCodeAt(0) - 44032) % 28
+
+// jongseong :: string -> JongseongCode
+// Given a string of zeros, returns its jongseong code.
+// e.g. jongseongZeros('00') === 1    (100 = 백)
+//   the number |
+//   of zeros   |
+//   -----------+-------
+//       1      |  십
+//       2      |  백
+//       3      |  천
+//     4 ~ 7    |  만
+//     8 ~ 11   |  억
+//    12 ~ 15   |  조
+//    16 ~ 19   |  경
+//    20 ~ 23   |  해
+const jongseongZeros = zs => {
+  const n = zs.length
+
+  if (n === 1)
+    return 17
+  if (n === 2 || (8 <= n && n <= 11))
+    return 1
+  if (3 <= n && n <= 7)
+    return 4
+  if ((12 <= n && n <= 15) || (20 <= n && n <= 23))
+    return 0
+  if (16 <= n && n <= 19)
+    return 21
+
+  throw new Error('It\'s too large.')
+}
+
+// jongseongDigit :: string -> JongseongCode
+// Given a digit, returns its jongseong code.
+// Assume d is one of: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+const jongseongDigit = d =>
+  [21, 8, 0, 16, 0, 0, 1, 8, 8, 0][d]
+
+// jongseongEnglish :: string -> JongseongCode
+// Given a two-letter English string, returns its jongseong code.
+// Assume e is a two-letter English string.
+const jongseongEnglish = e =>
+  /ck/i.test(e) ?  1 :
+  /.n/i.test(e) ?  4 :
+  /ne/i.test(e) ?  4 :
+  /.l/i.test(e) ?  8 :
+  /le/i.test(e) ?  8 :
+  /.m/i.test(e) ? 16 :
+  /ob/i.test(e) ? 17 :
+  /.p/i.test(e) ? 17 :
+  /et/i.test(e) ? 19 :
+  /ng/i.test(e) ? 21 :
+  /* else */       0
+
+// jongseongEnglishInitial : string -> JongseongCode
+// Given an English letter, returns its jongseong code.
+// Assume e is a single-letter English string.
+const jongseongEnglishInitial = e => {
+  switch (e.toLowerCase()) {
+    case 'l':
+    case 'r': return  8
+    case 'm': return 16
+    case 'n': return  4
+    default:  return  0
+  }
+}
+
+// jongseong : string -> JongseongCode
+// Computes the jongseong code of a given string.
+// If there isn't any recognizable letter in word, returns 0 (no jongseong).
+const jongseong = word => {
+  if (!word)
+    return 0
+
+  // !!!
+  // Ignore letters inside parentheses.
+  const w    = word.replace(/\([^)]*\)$/, '')
+  const last = w[w.length - 1]
+
+  if (/[가-힣]/.test(last))
+    return jongseongHangul(last)
+
+  if (/[1-9]0+$/.test(w)) {
+    const zerosMatch = /0+$/.exec(w)
+
+    return jongseongZeros(zerosMatch[0])
+  }
+
+  if (/\d/.test(last))
+    return jongseongDigit(last)
+
+  if (/[a-z]{2}$/i.test(w))
+    return jongseongEnglish(w.slice(w.length - 2, w.length))
+
+  if (/(?:^|[^a-z])[a-z]$/i.test(w))
+    return jongseongEnglishInitial(last)
+
+  if (/(^|[^a-z])[a-z][^a-z]?$/i.test(w))
+    return jongseongEnglishInitial(w[w.length - 2])
+
+  return jongseong(w.slice(0, w.length - 1))
+}
+
+// hasJongseong : string -> boolean
+// Does the last letter of a given word have a jongseong?
+const hasJongseong = w =>
+  jongseong(w) !== 0
+
+module.exports = { jongseongHangul
+                 , jongseongZeros
+                 , jongseongDigit
+                 , jongseongEnglish
+                 , jongseongEnglishInitial
+                 , jongseong
+                 , hasJongseong
+                 }
---
+++ src/index.test.js
@@ -0,0 +1,104 @@
+const { assert } = require('chai')
+
+const { jongseongHangul
+      , jongseongZeros
+      , jongseongDigit
+      , jongseongEnglish
+      , jongseongEnglishInitial
+      , jongseong
+      , hasJongseong
+      } = require('./index')
+
+describe('jongseongHangul()', () => {
+  it('computes the jongseong code of a Hangul letter', () => {
+    assert.equal(jongseongHangul('가'),  0)
+    assert.equal(jongseongHangul('납'), 17)
+    assert.equal(jongseongHangul('닿'), 27)
+  })
+})
+
+describe('jongseongZeros()', () => {
+  it('returns the jongseong code of a number that ends with a given string of zeros', () => {
+    assert.equal(jongseongZeros('0'),                    17)  // 십
+    assert.equal(jongseongZeros('00'),                    1)  // 백
+    assert.equal(jongseongZeros('000'),                   4)  // 천
+    assert.equal(jongseongZeros('0000'),                  4)  // 만
+    assert.equal(jongseongZeros('00000000'),              1)  // 억
+    assert.equal(jongseongZeros('000000000000'),          0)  // 조
+    assert.equal(jongseongZeros('0000000000000000'),     21)  // 경
+    assert.equal(jongseongZeros('00000000000000000000'),  0)  // 해
+  })
+
+  it('throws an error if there are too many zeros', () => {
+    assert.throws( () => { jongseongZeros('000000000000000000000000') }
+                 , /It's too large/
+                 )
+  })
+})
+
+describe('jongseongDigit()', () => {
+  it('returns the jongseong code of a given digit', () => {
+    assert.equal(jongseongDigit('0'), 21)
+    assert.equal(jongseongDigit('1'),  8)
+    assert.equal(jongseongDigit('2'),  0)
+    assert.equal(jongseongDigit('3'), 16)
+    assert.equal(jongseongDigit('4'),  0)
+    assert.equal(jongseongDigit('5'),  0)
+    assert.equal(jongseongDigit('6'),  1)
+    assert.equal(jongseongDigit('7'),  8)
+    assert.equal(jongseongDigit('8'),  8)
+    assert.equal(jongseongDigit('9'),  0)
+  })
+})
+
+describe('jongseongEnglish()', () => {
+  it('returns the jongseong code of a given two-letter English string', () => {
+    assert.equal(jongseongEnglish('ck'),  1)
+    assert.equal(jongseongEnglish('on'),  4)
+    assert.equal(jongseongEnglish('ne'),  4)
+    assert.equal(jongseongEnglish('al'),  8)
+    assert.equal(jongseongEnglish('le'),  8)
+    assert.equal(jongseongEnglish('om'), 16)
+    assert.equal(jongseongEnglish('up'), 17)
+    assert.equal(jongseongEnglish('et'), 19)
+    assert.equal(jongseongEnglish('ng'), 21)
+    assert.equal(jongseongEnglish('ob'), 17)
+  })
+})
+
+describe('jongseongEnglishInitial()', () => {
+  it('returns the jongseong code of a given English letter', () => {
+    assert.equal(jongseongEnglishInitial('l'),  8)
+    assert.equal(jongseongEnglishInitial('r'),  8)
+    assert.equal(jongseongEnglishInitial('m'), 16)
+    assert.equal(jongseongEnglishInitial('n'),  4)
+    assert.equal(jongseongEnglishInitial('x'),  0)
+  })
+})
+
+describe('jongseong()', () => {
+  it('computes the jongseong code of a given string', () => {
+    assert.equal(jongseong('탐정'),  21)
+    assert.equal(jongseong('500'),    1)
+    assert.equal(jongseong('7'),      8)
+    assert.equal(jongseong('coffee'), 0)
+    assert.equal(jongseong('L'),      8)
+    assert.equal(jongseong('A.P.I.'), 0)
+    assert.equal(jongseong('!@#'),    0)
+  })
+})
+
+describe('hasJongseong()', () => {
+  it('checks whether the last letter of a given word has a jongseong', () => {
+    assert.isTrue(hasJongseong('탐정'))
+    assert.isFalse(hasJongseong('코드'))
+    assert.isTrue(hasJongseong('apple'))
+    assert.isTrue(hasJongseong('tom'))
+    assert.isFalse(hasJongseong('code'))
+    assert.isFalse(hasJongseong('A.P.I.'))
+    assert.isTrue(hasJongseong('L'))
+    assert.isFalse(hasJongseong('12'))
+    assert.isTrue(hasJongseong('13'))
+    assert.isTrue(hasJongseong('120'))
+  })
+})
